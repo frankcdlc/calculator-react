b501beb08a3df760edbc1f0845d3e489ca06ea78
Frank Cano <frank.cano@utec.edu.pe>
2022-06-11 00:22:57 -0500

    ADD: button styles

--- calculator/src/components/button.jsx
+++ calculator/src/components/button.jsx
@@ -1,9 +1,51 @@
+import styled from "@emotion/styled"
 import React from 'react'
 
-function Button(props) {
-  const { dato } = props
+const cssColor = {
+  white:
+  {
+    backgroundColor: `white`
+  },
+  cyan:
+  {
+    backgroundColor: `#06B6D4`
+  },
+  gray:
+  {
+    backgroundColor: `#F3F4F6`
+  }
+}
+
+const cssButton = {
+  operator:
+  {
+    backgroundColor: ""
+  },
+  spanTwo:
+  {
+    gridRow: `span 2`,
+    color: `white`
+  }
+}
+
+function Button({styles, dato, color, type, ...props}) {
+  // const { dato, type } = props
+
+  const StyledButton = styled.button({
+    cursor: `pointer`,
+    fontSize: `2rem`,
+    color: `#4B5563`,
+    outline: `none`,
+    border: `1px solid rgb(235, 231, 231)`,
+    ...cssButton[type],
+    ...cssColor[color],
+    ...styles
+  })
+  
   return (
-    <button>{dato}</button>
+    <StyledButton {...props}>
+      {dato}
+    </StyledButton>
   )
 }
 
