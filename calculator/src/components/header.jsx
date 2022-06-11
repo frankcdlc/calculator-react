10a4d02b1fdc08250f65f3eac0a1cd60e1a7c245
Frank Cano <frank.cano@utec.edu.pe>
2022-06-11 00:22:00 -0500

    ADD: header styles

--- calculator/src/components/header.jsx
+++ calculator/src/components/header.jsx
@@ -1,8 +1,18 @@
+import styled from "@emotion/styled"
 import React from 'react'
 
 function Header() {
+  const StyledHeader = styled.div({
+    border: `1px solid rgb(235, 231, 231)`,
+    backgroundColor: `#06B6D4`,
+    color: `white`,
+    textAlign: `center`,
+    fontSize: `2rem`
+  })
+
+
   return (
-    <div>Header</div>
+    <StyledHeader>Groceries</StyledHeader>
   )
 }
 
